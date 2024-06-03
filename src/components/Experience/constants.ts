1c59bbbdfafb6cced3f727c88105fb6fac1291a4
Vomilas <safmail96@gmail.com>
2024-06-03 19:32:50 +0300

    update jobs

--- src/components/Experience/constants.ts
+++ src/components/Experience/constants.ts
@@ -13,12 +13,50 @@ export const JOBS = [
   {
     slug: "ibs",
     companyName: "IBS",
-    position: "Senior Frontend Engineer",
+    position: "Senior Frontend Developer",
     period: "May 2022 - Jan 2023",
     link: "ibs.com",
     tasks: [
-      "Worked with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Next.js/React",
+      "Develop the platform for remote education. The platform offered features for creating universal educational process for any school or university",
       "Interfaced with developers on a daily basis, providing technological expertise.",
+    ],
+  },
+  {
+    slug: "luxoft",
+    companyName: "Luxoft",
+    position: "Senior Frontend Developer",
+    period: "Jul 2020 - May 2022",
+    link: "luxoft.com",
+    tasks: [
+      "Develop the school and student accelerator for schoolchildren, college students, postgraduates, researchers at institutes and universities. Participants will be able to develop their business ideas and launch their first commercial project based on the platform.",
+      "Work in an Agile scrum team. Participate in all Agile ceremonies. Work closely with the scrum master to estimate effort of new features.",
+      "Organize team code-reviews and maintain code quality and best practices across the Front-End. Mentor and support other developers",
+    ],
+  },
+  {
+    slug: "uptrader",
+    companyName: "UpTrader",
+    position: "Senior Frontend Developer",
+    period: "Jul 2019 - Jul 2020",
+    link: "uptrader.io",
+    tasks: [
+      "Develop a cloud-based solution and CRM system for traders and brokers of different specialization and sizes",
+      "Work in an Agile scrum team. Participate in all Agile ceremonies. Work closely with the scrum master to estimate effort of new features",
+      "Continuously refactor and optimize the system to improve the efficiency and quality of the development cycles",
+      "Create optimized multi-language solutions across multiple devices and screens, including desktops, tablets, smartphones",
+    ],
+  },
+  {
+    slug: "digitalWing",
+    companyName: "Digital Wing",
+    position: "Frontend Developer",
+    period: "Mar 2019 - Aug 2019",
+    link: "2people.io",
+    tasks: [
+      "Managed the frontend development of the website and applications created for customers",
+      "Focus on performance, usability, and up-to-date practices",
+      "Work with micro-services teams on RESTful API designs - assist with future scripted API’s and websocket investigations",
+      "Collaborate with design and development teams to develop innovative ways to deliver optimized content based on the user`s personal",
     ],
   },
 ];
