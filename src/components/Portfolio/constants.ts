025c00b02615c0e65d3c0d275193e649047bdc4b
Vomilas <safmail96@gmail.com>
2024-06-07 10:02:49 +0300

    add student acc info

--- src/components/Portfolio/constants.ts
+++ src/components/Portfolio/constants.ts
@@ -1,9 +1,18 @@
 export const PROJECTS = [
   {
+    title: "Sber",
+    subTitle: "School & Student accelerator",
+    description:
+      "I had the opportunity to lead the UI development of the accelerator for schoolchildren, college students,postgraduates, researchers at institutes and universities. Participants will be able to develop their business ideas and launch their first commercial project based on the platform.",
+    link: "https://sberstudent.sberclass.ru",
+    imageSrc: "/assets/schoolacc.png",
+    keywords: ["Accelerator", "Education", "SberClass"],
+  },
+  {
     title: "UpTrader",
     subTitle: "UpTrader CRM",
     description:
-      "At UpTrader, I played a crucial role in developing ui components for their web application using Next.js and other libraries. Our focus was on integrating new features and improving the user interface.",
+      "At UpTrader, I played a crucial role in developing CRM system for traders and brokers. In a fairly short time, our team has managed to implement several main features as partnership systems, bonus management, trading tools etc, that are actively used by clients nowadays",
     link: "https://uptrader.io",
     imageSrc: "/assets/uptrader.webp",
     keywords: ["UpTrader", "Meta trading", "CRM"],
