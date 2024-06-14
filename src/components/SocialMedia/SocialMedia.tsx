ec34a50720e89e3df6425662278b25236283500d
Vomilas <safmail96@gmail.com>
2024-06-14 11:53:12 +0300

    update links

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -3,9 +3,9 @@ import { memo } from "react";
 import { TelegramIcon, LinkedinIcon, GithubIcon } from "../icons";
 
 const MEDIAS = [
-  { Icon: TelegramIcon, url: "#" },
-  { Icon: LinkedinIcon, url: "#" },
-  { Icon: GithubIcon, url: "#" },
+  { Icon: TelegramIcon, url: "https://t.me/vomilas" },
+  { Icon: LinkedinIcon, url: "https://linkedin.com/vomilas" },
+  { Icon: GithubIcon, url: "https://github.com/Vomilas" },
 ];
 
 export const SocialMedia = memo(() => {
@@ -14,10 +14,9 @@ export const SocialMedia = memo(() => {
       <div className="flex flex-col items-center justify-center space-y-8">
         <div className="flex flex-col items-center justify-center space-y-5">
           {MEDIAS.map(({ Icon, url }) => (
-            <Icon
-              key={url}
-              className="h-6 w-6 fill-current text-gray-400 hover:cursor-pointer hover:text-secondary"
-            />
+            <a href={url} key={url}>
+              <Icon className="h-6 w-6 fill-current text-gray-400 hover:cursor-pointer hover:text-secondary" />
+            </a>
           ))}
         </div>
         <div className="h-28 w-0.5 bg-gray-400"></div>
